f338104fedca38d4499a7028b0056b1f01aa3f1b
Gipsonek123 <134952200+Gipsonek123@users.noreply.github.com>
2026-01-24 18:34:38 +0100

    Improved validators for login and registration

--- AngularWebApp.Server/Dtos/Requests/RegisterDto.cs
+++ AngularWebApp.Server/Dtos/Requests/RegisterDto.cs
@@ -5,11 +5,11 @@ namespace AngularWebApp.Server.Dtos.Requests
     public class RegisterDto
     {
         [Required(ErrorMessage = "Username is required")]
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Min 5 or max 20 characters allowed")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Min 4 or max 20 characters allowed")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Min 5 or max 20 characters allowed")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Min 6 or max 20 characters allowed")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirm password is required")]
--- angularwebapp.client/src/app/core/validators/custom-validators.ts
+++ angularwebapp.client/src/app/core/validators/custom-validators.ts
@@ -1,7 +1,7 @@
 import { AbstractControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 export class CustomValidators {
-  static readonly PASSWORD_PATTERN = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])/;
+  static readonly PASSWORD_PATTERN = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])/;
   static readonly PASSWORD_CONTROL = 'password';
   static readonly CONFIRM_PASSWORD_CONTROL = 'confirmPassword';
 
